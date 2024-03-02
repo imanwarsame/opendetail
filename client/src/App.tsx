01aa801b013802258978fb1dcda8bec001eb1a87
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-03-02 15:29:36 +0100

    showing list of projects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { DetailSearch } from './features/search/DetailSearch';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<div>
+			<DetailSearch objects={[]} />
+			{/* <div>
 				<a href='https://vitejs.dev' target='_blank'>
 					<img src={viteLogo} className='logo' alt='Vite logo' />
 				</a>
@@ -23,7 +25,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p> */}
 		</>
 	);
 }
