3adb50817b6f7b6c4e9eb8dc1641abb16bcdfb10
Sylvain Usai <usai@designtoproduction.com>
2024-03-02 21:39:03 +0100

    removed detailParts from elementObject

--- client/src/types/elementobject.ts
+++ client/src/types/elementobject.ts
@@ -1,8 +1,7 @@
 import { IBuildupPart } from './builduppart';
-import { IDetailPart } from './detailpart';
 
 export interface IElementObject {
 	BuildupParts: IBuildupPart[];
-	DetailParts: IDetailPart[];
+
 }
 
