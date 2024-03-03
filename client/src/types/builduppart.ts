89c0ed9e4f291e7068ccda3d8293c998165b6d81
Sylvain Usai <usai@designtoproduction.com>
2024-03-03 01:45:39 +0100

    added witty comment about index starting at 1

--- client/src/types/builduppart.ts
+++ client/src/types/builduppart.ts
@@ -3,8 +3,9 @@ import { MaterialCategory } from '../enums/materialcategory';
 
 export interface IBuildupPart {
 	// A buildup part is essentially a layer in the element sandwich
-	
+
 	index: number; // the "order" of this part in the buildup. Multiple parts can have the same index and then they would be merged in the same layer
+	// INDEX STARTS AT 1 ! (because I decided so...)
 	materialCategory: MaterialCategory; // first level material category
 	materialName : string; // human readable material name, isn't standardized, use with caution
 	thickness: number; // the thickness of this layer
