2948cc96ab9961823700038d37d8d79ef0adf808
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-03-03 12:10:26 +0100

    new added date method

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { UserRole } from '../enums/userroles';
 import { IDetailObject } from '../types/detailobject';
-import { createDetail } from '../data/boilerplate';
+import { createDetails } from '../data/boilerplate';
 
 export interface State {
 	selectedDetail: string | null;
@@ -24,7 +24,7 @@ export const useDetailStore = create(
 		const state: State = {
 			selectedDetail: null,
 			userRole: UserRole.Admin,
-			allDetails: [...Array(20)].map(() => createDetail())
+			allDetails: createDetails(20)
 		};
 
 		const reducers: Reducers = {
