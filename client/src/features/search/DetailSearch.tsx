f09e417880403086139a59fa11a81dd23798622e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-03-03 09:10:48 +0100

    link to composite search when clicking on landing page

--- client/src/features/search/DetailSearch.tsx
+++ client/src/features/search/DetailSearch.tsx
@@ -1,6 +1,7 @@
 import { DetailCard } from './DetailCard';
 import './detailcard.css';
 import { useDetailStore } from '../../store';
+import { useNavigate } from 'react-router-dom';
 
 interface IDetailSearchProps {
 	style?: React.CSSProperties;
@@ -8,11 +9,19 @@ interface IDetailSearchProps {
 
 export const DetailSearch: React.FC<IDetailSearchProps> = ({ style }) => {
 	const { state, reducers } = useDetailStore();
+	const navigate = useNavigate();
 
 	return (
 		<div className='detail-cards' style={style}>
 			{state.allDetails.map((object, index) => (
-				<DetailCard key={index} object={object} onClick={() => reducers.setSelectedDetail(object.id)} />
+				<DetailCard
+					key={index}
+					object={object}
+					onClick={() => {
+						navigate(`/compositesearch/${object.id}`);
+						reducers.setSelectedDetail(object.id);
+					}}
+				/>
 			))}
 		</div>
 	);
