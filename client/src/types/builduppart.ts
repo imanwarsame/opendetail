0e611aec9d53189ba00e159515b3f6cb036ca19a
Sylvain Usai <usai@designtoproduction.com>
2024-03-03 01:35:49 +0100

    added desciption to each entries

--- client/src/types/builduppart.ts
+++ client/src/types/builduppart.ts
@@ -2,8 +2,11 @@ import { ElementRole } from '../enums/elementsroles';
 import { MaterialCategory } from '../enums/materialcategory';
 
 export interface IBuildupPart {
-	index: number;
-	materialCategory: MaterialCategory;
-	thickness: number;
-	elementRole: ElementRole;
+	// A buildup part is essentially a layer in the element sandwich
+	
+	index: number; // the "order" of this part in the buildup. Multiple parts can have the same index and then they would be merged in the same layer
+	materialCategory: MaterialCategory; // first level material category
+	materialName : string; // human readable material name, isn't standardized, use with caution
+	thickness: number; // the thickness of this layer
+	elementRole: ElementRole; // the function of this layer
 }
